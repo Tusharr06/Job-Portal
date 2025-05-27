56625871a0a7f7e7776fdc13bea614d1c150b45a
Tushar R <tusharcodes13@gmail.com>
2025-05-27 09:18:56 +0530

    Create HomePage component

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold text-gray-900 mb-8">Welcome to JobConnect</h1>
+      <p className="text-lg text-gray-700">
+        Find your next career opportunity or post a job to reach talented professionals.
+      </p>
+    </div>
+  )
+}
+
+export default HomePage
